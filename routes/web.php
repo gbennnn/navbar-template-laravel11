3d8438fcd70b3a05de886ec2d08d04f2ec0829c7
Benony Gabriel <gbenn.work@gmail.com>
2024-12-17 23:46:32 +0700

    Add new routes for blogs, about, contact, and privacy policy pages

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+Route::get('/blogs', function () {
+    return view('pages.blogs');
+})->name('blogs');
+
+Route::get('/about', function () {
+    return view('pages.about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('pages.contact');
+})->name('contact');
+
+Route::get('/privacy-policy', function () {
+    return view('pages.privacy-policy');
+})->name('privacy-policy');
