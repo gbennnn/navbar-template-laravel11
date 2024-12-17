98a1f58640a5dfe1b07b9edd5bef35156f52b4cd
Benony Gabriel <gbenn.work@gmail.com>
2024-12-18 00:06:08 +0700

    Update dependencies and add layout components for navigation

---
+++ resources/views/components/layout/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Finsweet | {{ $title ?? 'Finsweet' }}</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-white">
+
+    {{-- Navbar --}}
+    <x-navbar></x-navbar>
+
+
+    {{-- main section --}}
+    <main>
+        {{ $slot }}
+    </main>
+
+    {{-- Footer --}}
+
+
+    {{-- JS --}}
+    <script src="{{ asset('asset/js/script.js') }}"></script>
+
+    {{-- Ionics Icons --}}
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
